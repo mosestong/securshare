10fe04223b8c57171703d81e98509a8fa9e48c59
Moses Tong <mosestong2003@gmail.com>
2023-08-25 14:34:37 -0300

    Implemented encrypted file transfer

--- receiver.py
+++ receiver.py
@@ -4,6 +4,28 @@ from nacl.public import PrivateKey, PublicKey, Box
 import socket
 import os
 import builtins
+import logging
+import tqdm
+
+logging.basicConfig(filename='receiver.log', level=logging.INFO)
+
+# For decrypting ordinary messages
+def decrypt(message):
+    alice_box = Box(skalice, pkbob)
+    return alice_box.decrypt(message).decode()
+
+# For decrypting bytes, does not decode bytes to string
+def decryptbytes(bytes):
+    alice_box = Box(skalice, pkbob)
+    return alice_box.decrypt(bytes)
+
+port = 8080
+BUFFER_SIZE = 4096
+SEPARATOR = "<SEPARATOR>"
+# Device's own ip address
+# Using 0.0.0.0 to be reachable if device has multiple ip addresses
+device_ip = "0.0.0.0"
+# device_ip = socket.gethostbyname(socket.gethostname())
 
 # Generate keys for Alice
 skalice = PrivateKey.generate()
@@ -12,34 +34,26 @@ print("Skalice:", skalice)
 print("Pkalice:", pkalice)
 
 
-""" Could create this as a function??"""
+# TODO: Could create this as a function??
 
 # Create a server side socket using IPV4 (AF_INET) and TCP (SOCK_STREAM)
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# use dynamic port
-port = 8080
-# get host name
-host = socket.gethostname()
-
-# Get IP address dynamically by using the host name
-ip_addr = socket.gethostbyname(host)
-
-print("Host name:", host)
-print("IP address:", ip_addr)
-
-# Bind socket to tuple (IP address, Port address)
-server_socket.bind((ip_addr, port))
+# Bind socket to our local address (IP address, Port)
+server_socket.bind((device_ip, port))
 
 # Put socket into listening mode to listen for any possible connections
 server_socket.listen()
+print(f"[*] Listening as {device_ip}:{port}")
+# Accept any incoming connection if any
 client_socket, client_address = server_socket.accept()
+# Message printed if sender is connected
+print(f"[+] {client_address} is connected")
 
 # Send message to client that just connected
-client_socket.send(f"Connected to {host} with IP address of {ip_addr}".encode())
+client_socket.send(f"Connected to IP address of {socket.gethostbyname(socket.gethostname())}".encode())
 # Recieve "connected" message from the server
-# while True:
-message = client_socket.recv(1024).decode()
+message = client_socket.recv(BUFFER_SIZE).decode()
 
 if message == "quit":
     client_socket.send("quit".encode())
@@ -51,23 +65,53 @@ else:
 # Exchange public keys
 pkalice_encoded = pkalice.encode()
 # print("PKALICE_ENCODED:", pkalice_encoded)
-client_socket.send(pkalice_encoded)
-pkbob_encoded = client_socket.recv(1024)
-# print(f"\n{pkbob_encoded}")
+client_socket.sendall(pkalice_encoded)
+print("PKALICE_ENCODED SENT", pkalice_encoded)
+pkbob_encoded = client_socket.recv(BUFFER_SIZE)
+print(f"\nPKBOB RECEIVED {pkbob_encoded}")
 
 # Decode
 pkbob = PublicKey(pkbob_encoded)
 
-# RECIEVE ENCRYPTED FILE SIZE FIRST TO KNOW WHAT APPROPRIATE BUFSIZE SHOULD BE
-
-# Recieve encrypted file/message
-encrypted = client_socket.recv(1024)
-
-# print("ENCRYPTED MESSAGE", encrypted)
+# Receive encrypted file info
+file_info_enc = client_socket.recv(BUFFER_SIZE)
+print("FILE INFO ENC", file_info_enc)
 
 # Decrypt message using private key
-alice_box = Box(skalice, pkbob)
-plaintext = alice_box.decrypt(encrypted)
-print(plaintext.decode())
+file_info = decrypt(file_info_enc)
+print(file_info)
+
+file_name, file_size = file_info.split(SEPARATOR)
+
+# Remove absolute file path if present
+file_name = os.path.basename(file_name)
+
+# FOR TESTING PURPOSES: appending 'sent' to file name so that original file won't be overwritten
+file_name = "".join(["sent-", file_name])
+
+# Convert to integer
+file_size = int(file_size)
+print("FILE NAME", file_name)
+print("FILE SIZE", file_size)
+
+### PROBLEMS START!
+
+# Start receiving file from socket and writing to the file stream
+progress = tqdm.tqdm(range(file_size), f"Receiving {file_name}", unit="B", unit_scale=True, unit_divisor=1024)
+with open(file_name, "wb") as f:
+    while True:
+        # Receive and read 4096 bytes from the socket
+        data_enc = client_socket.recv(BUFFER_SIZE)
+        print("DATA_ENC:", data_enc)
+        if not data_enc:
+            # Nothing received and file transmitting is finished
+            break
+        # Decrypt bytes using keys
+        data = decryptbytes(data_enc)
+        print("DATA", data)
+        # Write to the file the bytes we received
+        f.write(data)
+        # Update progress bar
+        progress.update(len(data))
 
 server_socket.close()
--- sender.py
+++ sender.py
@@ -4,11 +4,34 @@ from nacl.public import PrivateKey, PublicKey, Box
 import socket
 import os
 import builtins
+import tqdm
 
+# For sending ordinary messages
 def send(message):
     bob_box = Box(skbob, pkalice_decoded)
     encrypted = bob_box.encrypt(message.encode())
-    client_socket.send(encrypted)
+    print("FILE INFO ENC", encrypted)
+    client_socket.sendall(encrypted)
+
+# For sending bytes
+def sendbytes(bytes):
+    bob_box = Box(skbob, pkalice_decoded)
+    encrypted = bob_box.encrypt(bytes)
+    client_socket.sendall(encrypted)
+
+# use dynamic port
+port = 8080
+# Host name of the receiver you want to send file to
+
+# TODO: Will use input() to get file name after
+
+file_name = "file.txt"
+file_size = os.path.getsize(file_name)
+SEPARATOR = "<SEPARATOR>"
+# TODO: Possibly allow to change buffer size? 
+# prob not though since client and sender would 
+# need to have same buf size
+BUFFER_SIZE = 4096
 
 # Generate keys for Bob
 skbob = PrivateKey.generate()
@@ -19,21 +42,23 @@ print("Pkbob:", pkbob)
 # Create client side IPV4 socket (AF_INET) and TCP (SOCK_STREAM)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# use dynamic port
-port = 8080
-host = socket.gethostname()
+# TODO: To be removed in favour of input(), only for testing right now so ip address is set to the same machine
+ip_addr = socket.gethostbyname(socket.gethostname())
+# ip_addr = input("Enter IP address to send file to: ")
 
-# Get IP address dynamically by using the host name
-ip_addr = socket.gethostbyname(host)
+# if ip_addr == "loopback":
+#     ip_addr = "localhost"
 
 # Connect the socket to a server located at a given IP and Port
+print(f"[+] Connecting to {ip_addr}:{port}")
 client_socket.connect((ip_addr, port))
+print(f"[+] Connected")
 
 # Send message to client that just connected
-client_socket.send(f"Connected to {host} with IP address of {ip_addr}".encode())
+client_socket.send(f"Connected to IP address of {socket.gethostbyname(socket.gethostname())}".encode())
 # Recieve "connected" message from the server
-# while True:
-message = client_socket.recv(1024).decode()
+message = client_socket.recv(BUFFER_SIZE).decode()
+print(message)
 
 if message == "quit":
     client_socket.send("quit".encode())
@@ -44,15 +69,37 @@ else:
 
 # Exchange public keys
 pkbob_encoded = pkbob.encode()
-print("PKBOB_ENCODED:", pkbob_encoded)
-client_socket.send(pkbob_encoded)
-pkalice_decoded = client_socket.recv(1024)
-print(f"\n{pkalice_decoded}")
+client_socket.sendall(pkbob_encoded)
+print("PKBOB_ENCODED SENT:", pkbob_encoded)
+pkalice_decoded = client_socket.recv(BUFFER_SIZE)
+print(f"\nPKALICE RECEIVED {pkalice_decoded}")
+
+# TODO: Exception if PKALICE is empty, ie not sent correctly
 
 # Decode
 pkalice_decoded = PublicKey(pkalice_decoded)
 
-send("hello")
+# Send the name and size of the file
+send(f"{file_name}{SEPARATOR}{file_size}")
+print("FILE NAME", file_name)
+print("FILE SIZE", file_size)
+
+
+### PROBLEMS START! 
+
+# start sending file
+progress = tqdm.tqdm(range(file_size), f"Sending {file_name}", unit="B", unit_scale=True, unit_divisor=1024)
+
+# Read file in chunks and send to socket
+with open(file_name, "rb") as f:
+    while True:
+        data = f.read(BUFFER_SIZE)
+        print("DATA", data)
+        if not data:
+            # file transmitting is done
+            break
+        sendbytes(data)
+        progress.update(len(data))
 
 client_socket.close()
 
