4d35191b223eb96fd1a36ca4c0368fb1758e0fa5
Moses Tong <mosestong2003@gmail.com>
2023-08-26 21:31:18 -0300

    Restructured code so that code is under a function

--- receiver.py
+++ receiver.py
@@ -11,135 +11,118 @@ logging.basicConfig(filename='receiver.log', level=logging.INFO)
 logging.info("\nNew run:\n")
 
 # For sending ordinary messages
-def send(message):
+def send(client_socket, skalice, pkbob, message):
     alice_box = Box(skalice, pkbob)
     encrypted = alice_box.encrypt(message.encode())
-    print("ENC MESSAGE", encrypted)
+    # print("ENC MESSAGE", encrypted)
     client_socket.sendall(encrypted)
 
 # For decrypting ordinary messages
-def decrypt(message):
+def decrypt(skalice, pkbob, message):
     alice_box = Box(skalice, pkbob)
     return alice_box.decrypt(message).decode()
 
 # For decrypting bytes, does not decode bytes to string
-def decryptbytes(bytes):
+def decryptbytes(skalice, pkbob, bytes):
     alice_box = Box(skalice, pkbob)
     return alice_box.decrypt(bytes)
 
-port = 8080
+def receiver(BUFFER_SIZE=1064):
+    port = 8080
+    # NOTE: Buffer size must always be 40 bytes more than the sender because encryption will pad with 40 more bytes automatically
+    BUFFER_SIZE = 1064
+    SEPARATOR = "<SEPARATOR>"
+    # Using 0.0.0.0 to be reachable if device has multiple ip addresses
+    device_ip = "0.0.0.0"
 
-# NOTE: Buffer size must always be 40 bytes more than the sender because encryption will pad with 40 more bytes automatically
+    # Generate keys for Alice
+    skalice = PrivateKey.generate()
+    pkalice = skalice.public_key
+    print("Skalice:", skalice)
+    print("Pkalice:", pkalice)
 
-BUFFER_SIZE = 1064
-SEPARATOR = "<SEPARATOR>"
-# Device's own ip address
-# Using 0.0.0.0 to be reachable if device has multiple ip addresses
-device_ip = "0.0.0.0"
-# device_ip = socket.gethostbyname(socket.gethostname())
+    # TODO: Could create this as a function??
 
-# Generate keys for Alice
-skalice = PrivateKey.generate()
-pkalice = skalice.public_key
-print("Skalice:", skalice)
-print("Pkalice:", pkalice)
+    # Create a server side socket using IPV4 (AF_INET) and TCP (SOCK_STREAM)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# TODO: Could create this as a function??
+    # Bind socket to our local address (IP address, Port)
+    server_socket.bind((device_ip, port))
 
-# Create a server side socket using IPV4 (AF_INET) and TCP (SOCK_STREAM)
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Put socket into listening mode to listen for any possible connections
+    server_socket.listen()
+    print(f"[*] Listening as {device_ip}:{port}")
+    # Accept any incoming connection if any
+    client_socket, client_address = server_socket.accept()
+    # Message printed if sender is connected
+    print(f"[+] {client_address} is connected")
 
-# Bind socket to our local address (IP address, Port)
-server_socket.bind((device_ip, port))
+    # Exchange public keys
+    pkalice_encoded = pkalice.encode()
+    client_socket.sendall(pkalice_encoded)
+    print("PKALICE_ENCODED SENT", pkalice_encoded)
+    pkbob = client_socket.recv(BUFFER_SIZE)
+    print(f"\nPKBOB RECEIVED {pkbob}")
 
-# Put socket into listening mode to listen for any possible connections
-server_socket.listen()
-print(f"[*] Listening as {device_ip}:{port}")
-# Accept any incoming connection if any
-client_socket, client_address = server_socket.accept()
-# Message printed if sender is connected
-print(f"[+] {client_address} is connected")
+    # Convert bytes back into PublicKey object
+    pkbob = PublicKey(pkbob)
 
-# NOTE: Removed because connect message and public key data have possibility of merging
-    # Would need to send "OK" confirmation before sending key data
+    # Receive encrypted file info
+    file_info_enc = client_socket.recv(BUFFER_SIZE)
+    print("FILE INFO ENC", file_info_enc)
 
-# # Send message to client that just connected
-# client_socket.sendall(f"Connected to IP address of {socket.gethostbyname(socket.gethostname())}".encode())
-# # Recieve "connected" message from the server
-# message = client_socket.recv(BUFFER_SIZE).decode()
+    # Decrypt message using private key
+    file_info = decrypt(skalice, pkbob, file_info_enc)
+    file_name, file_size = file_info.split(SEPARATOR)
 
-# if message == "quit":
-#     client_socket.send("quit".encode())
-#     print("\nEnding the chat...goodbye!")
-#     # break
-# else:
-#     print(f"\n{message}")
+    # Remove absolute file path if present
+    file_name = os.path.basename(file_name)
 
-# Exchange public keys
-pkalice_encoded = pkalice.encode()
-client_socket.sendall(pkalice_encoded)
-print("PKALICE_ENCODED SENT", pkalice_encoded)
-pkbob = client_socket.recv(BUFFER_SIZE)
-print(f"\nPKBOB RECEIVED {pkbob}")
+    # FOR TESTING PURPOSES: appending 'sent' to file name so that original file won't be overwritten
+    file_name = "".join(["sent-", file_name])
 
-# Convert bytes back into PublicKey object
-pkbob = PublicKey(pkbob)
-
-# Receive encrypted file info
-file_info_enc = client_socket.recv(BUFFER_SIZE)
-print("FILE INFO ENC", file_info_enc)
-
-# Decrypt message using private key
-file_info = decrypt(file_info_enc)
-print(file_info)
-
-file_name, file_size = file_info.split(SEPARATOR)
-
-# Remove absolute file path if present
-file_name = os.path.basename(file_name)
-
-# FOR TESTING PURPOSES: appending 'sent' to file name so that original file won't be overwritten
-file_name = "".join(["sent-", file_name])
-
-# Convert to integer
-file_size = int(file_size)
-print("FILE NAME", file_name)
-print("FILE SIZE", file_size)
+    print(f"FILE SIZE TYPE {type(file_size)}")
+    # Convert to integer
+    file_size = int(file_size)
+    print("FILE NAME", file_name)
+    print("FILE SIZE", file_size)
 
 ##### TEST
 # Send ok before transmitting file to ensure clients are in sync and to avoid mixing packets together/out-of-order delivery
 # OR: Append <END> label to every packet to know when packet ends and remove once accepted?
-send("OK")
+    send(client_socket, skalice, pkbob, "OK")
 
 # Start receiving file from socket and writing to the file stream
-progress = tqdm.tqdm(range(file_size), f"Receiving {file_name}", unit="B", unit_scale=True, unit_divisor=1064, total=int(file_size * 1.04))
-file_bytes = b""
+    progress = tqdm.tqdm(range(file_size), f"Receiving {file_name}", unit="B", unit_scale=True, unit_divisor=1064, total=int(file_size * 1.04))
+    file_bytes = b""
 
-i = 0
-with open(file_name, "wb") as f:
-    while True:
-        # Receive and read x bytes from the socket
-        data_enc = client_socket.recv(BUFFER_SIZE)
-        # No more data to transmit
-        if not data_enc:
-            logging.info(f"Received empty")
-            break
-        # logging.info(f"DATA_ENC: {data_enc}")
-        # # Decrypt bytes using keys
-        try:
-            data = decryptbytes(data_enc)
-        except nacl.exceptions.CryptoError:
-            logging.info(f"{i}: Failed: {data_enc}")
-        else:
-            logging.info(f"{i}: Success")
-        finally:
-            i += 1
-        # print("DECRYPTED", data)
-        file_bytes += data
-        # Update progress bar
-        progress.update(len(data_enc))
+    i = 0
+    with open(file_name, "wb") as f:
+        while True:
+            # Receive and read x bytes from the socket
+            data_enc = client_socket.recv(BUFFER_SIZE)
+            # No more data to transmit
+            if not data_enc:
+                logging.info(f"Received empty")
+                break
+            # logging.info(f"DATA_ENC: {data_enc}")
+            try:
+                data = decryptbytes(skalice, pkbob, data_enc)
+            except nacl.exceptions.CryptoError:
+                logging.info(f"{i}: Failed: {data_enc}")
+            else:
+                logging.info(f"{i}: Success")
+            finally:
+                i += 1
+            # print("DECRYPTED", data)
+            file_bytes += data
+            # Update progress bar
+            progress.update(len(data_enc))
 
-    # Write to file decrypted bytes
-    f.write(file_bytes)
+        # Write to file decrypted bytes
+        f.write(file_bytes)
 
-server_socket.close()
+    server_socket.close()
+
+if __name__ == "__main__":
+    receiver()
--- sender.py
+++ sender.py
@@ -11,14 +11,14 @@ logging.basicConfig(filename='sender.log', level=logging.INFO)
 logging.info("\nNew run:\n")
 
 # For sending ordinary messages
-def send(message):
+def send(client_socket, skbob, pkalice, message):
     bob_box = Box(skbob, pkalice)
     encrypted = bob_box.encrypt(message.encode())
-    print("FILE INFO ENC", encrypted)
+    # print("FILE INFO ENC", encrypted)
     client_socket.sendall(encrypted)
 
 # For sending bytes
-def sendbytes(bytes, i):
+def sendbytes(client_socket, skbob, pkalice, bytes, i):
     bob_box = Box(skbob, pkalice)
     # print("BEFORE ENCRYPT", len(bytes))
     encrypted = bob_box.encrypt(bytes)
@@ -27,107 +27,87 @@ def sendbytes(bytes, i):
     logging.info(f"{i}: {encrypted}")
 
 # For decrypting ordinary messages
-def decrypt(message):
+def decrypt(skbob, pkalice, message):
     bob_box = Box(skbob, pkalice)
     return bob_box.decrypt(message).decode()
 
-# use dynamic port
-port = 8080
-# Host name of the receiver you want to send file to
+def sender():
+    port = 8080
 
-# TODO: Will use input() to get file name after
+    # TODO: Will use input() to get file name after
+    file_name = "tower.jpg"
+    file_size = os.path.getsize(file_name)
+    SEPARATOR = "<SEPARATOR>"
 
-file_name = "tower.jpg"
-# file_name = "moses-tong-resume.pdf"
-# file_name = "proxy-image.jpg"
-# file_name = "file.txt"
-file_size = os.path.getsize(file_name)
-SEPARATOR = "<SEPARATOR>"
-# TODO: Possibly allow to change buffer size? 
-# prob not though since client and sender would 
-# need to have same buf size
-BUFFER_SIZE = 1024
+    # TODO: Possibly allow to change buffer size? 
+    # prob not though since client and sender would 
+    # need to have same buf size
 
-# Generate keys for Bob
-skbob = PrivateKey.generate()
-pkbob = skbob.public_key
-print("Skbob:", skbob)
-print("Pkbob:", pkbob)
+    BUFFER_SIZE = 1024
+
+    # Generate keys for Bob
+    skbob = PrivateKey.generate()
+    pkbob = skbob.public_key
+    print("Skbob:", skbob)
+    print("Pkbob:", pkbob)
 
 # Create client side IPV4 socket (AF_INET) and TCP (SOCK_STREAM)
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# TODO: To be removed in favour of input(), only for testing right now so ip address is set to the same machine
-ip_addr = socket.gethostbyname(socket.gethostname())
-# ip_addr = input("Enter IP address to send file to: ")
+    # TODO: To be removed in favour of input(), only for testing right now so ip address is set to the same machine
+    # Ip address you want to send to 
+    ip_addr = socket.gethostbyname(socket.gethostname())
+    # ip_addr = input("Enter IP address to send file to: ")
 
-# if ip_addr == "loopback":
-#     ip_addr = "localhost"
+    # if ip_addr == "loopback":
+    #     ip_addr = "localhost"
 
 # Connect the socket to a server located at a given IP and Port
-print(f"[+] Connecting to {ip_addr}:{port}")
-client_socket.connect((ip_addr, port))
-print(f"[+] Connected")
-
-# NOTE: Removed because connect message and public key data have possibility of merging
-    # Would need to send "OK" confirmation before sending key data
-
-# # Send message to client that just connected
-# client_socket.sendall(f"Connected to IP address of {socket.gethostbyname(socket.gethostname())}".encode())
-# # Recieve "connected" message from the server
-# try:
-#     message = client_socket.recv(BUFFER_SIZE)
-#     print(message.decode())
-# except UnicodeDecodeError:
-#     print("Received message couldn't be decoded as UTF-8")
-#     print(f"Raw bytes: {message}")
-
-# if message == "quit":
-#     client_socket.send("quit".encode())
-#     print("\nEnding the chat...goodbye!")
-#     # break
-# else:
-#     print(f"\n{message}")
+    print(f"[+] Connecting to {ip_addr}:{port}")
+    client_socket.connect((ip_addr, port))
+    print(f"[+] Connected")
 
 # Exchange public keys
-pkbob_encoded = pkbob.encode()
-client_socket.sendall(pkbob_encoded)
-print("PKBOB_ENCODED SENT:", pkbob_encoded)
-pkalice = client_socket.recv(BUFFER_SIZE)
-print(f"\nPKALICE RECEIVED {pkalice}")
+    pkbob_encoded = pkbob.encode()
+    client_socket.sendall(pkbob_encoded)
+    print("PKBOB_ENCODED SENT:", pkbob_encoded)
+    pkalice = client_socket.recv(BUFFER_SIZE)
+    print(f"\nPKALICE RECEIVED {pkalice}")
 
 # Convert bytes back into PulicKey object
-pkalice = PublicKey(pkalice)
+    pkalice = PublicKey(pkalice)
 
 # Send the name and size of the file
-send(f"{file_name}{SEPARATOR}{file_size}")
-print("FILE NAME", file_name)
-print("FILE SIZE", file_size)
+    send(client_socket, skbob, pkalice, f"{file_name}{SEPARATOR}{file_size}")
+    print("FILE NAME", file_name)
+    print("FILE SIZE", file_size)
 
 # Receive "OK" message before sending file
-ok_enc = client_socket.recv(BUFFER_SIZE)
-print("OK ENC", ok_enc)
+    ok_enc = client_socket.recv(BUFFER_SIZE)
+    print("OK ENC", ok_enc)
 
 # Decrypt ok message using private key
-ok = decrypt(ok_enc)
-print("OK", ok)
+    ok = decrypt(skbob, pkalice, ok_enc)
+    print("OK", ok)
 
 # Start sending file once recieved confirmation of ready to accept
-if ok == "OK":
-    # start sending file
-    progress = tqdm.tqdm(range(file_size), f"Sending {file_name}", unit="B", unit_scale=True, unit_divisor=1024, total=int(file_size))
-    i = 0
-    # Read file in chunks and send to socket
-    with open(file_name, "rb") as f:
-        while True:
-            data = f.read(BUFFER_SIZE)
-            if not data:
-                break
-            sendbytes(data, i)
-            i += 1
-            progress.update(len(data))
-else:
-    print(f"Did not receive OK. File transfer failed.")
+    if ok == "OK":
+        # start sending file
+        progress = tqdm.tqdm(range(file_size), f"Sending {file_name}", unit="B", unit_scale=True, unit_divisor=1024, total=int(file_size))
+        i = 0
+        # Read file in chunks and send to socket
+        with open(file_name, "rb") as f:
+            while True:
+                data = f.read(BUFFER_SIZE)
+                if not data:
+                    break
+                sendbytes(client_socket, skbob, pkalice, data, i)
+                i += 1
+                progress.update(len(data))
+    else:
+        print(f"Did not receive OK. File transfer failed.")
 
-client_socket.close()
+    client_socket.close()
 
+if __name__ == "__main__":
+    sender()
